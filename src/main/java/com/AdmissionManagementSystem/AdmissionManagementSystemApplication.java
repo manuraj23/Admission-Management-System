5ab23989b9403a901383648c776a084f1f34dd30
manuraj23 <manuraj082004@gmail.com>
2025-05-28 16:26:05 +0530

    Docker Files and compose added

--- src/main/java/com/AdmissionManagementSystem/AdmissionManagementSystemApplication.java
+++ src/main/java/com/AdmissionManagementSystem/AdmissionManagementSystemApplication.java
@@ -1,5 +1,4 @@
 package com.AdmissionManagementSystem;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,12 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class AdmissionManagementSystemApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			if (System.getProperty(entry.getKey()) == null) {
-				System.setProperty(entry.getKey(), entry.getValue());
-			}
-		});
 		SpringApplication.run(AdmissionManagementSystemApplication.class, args);
 	}
 
